503ed2092436b3a757234321f2d2df88ca9665f3
Alex <alex.nowik@mail.ru>
2023-02-09 22:10:33 +0300

    Initial preparations (Base test + POM file)

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,18 @@
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class BaseTest {
+    protected Browser browser;
+
+    @BeforeTest
+    public void setUp(){
+        browser = AqualityServices.getBrowser();
+        browser.maximize();
+    }
+    @AfterTest
+    public void tearDown(){
+        browser.quit();
+    }
 }
