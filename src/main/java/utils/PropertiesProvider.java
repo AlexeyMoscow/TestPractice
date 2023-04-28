c219255ad5255d8dbf718ab9f331b57b5a407760
Alex <alex.nowik@mail.ru>
2023-04-28 18:52:59 +0300

    Properties loader stable

---
+++ src/main/java/utils/PropertiesProvider.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesProvider {
+    private static final Properties PROPERTIES = new Properties();
+
+    private PropertiesProvider() {
+    }
+
+    static {
+        loadProperties();
+    }
+
+    private static void loadProperties() {
+        try(var inputStream = PropertiesProvider.class.getClassLoader().getResourceAsStream("application.properties")){
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getFromProperties(String propertiesKey){
+        return PROPERTIES.getProperty(propertiesKey);
+    }
+}
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,10 @@
+import org.testng.annotations.Test;
+import utils.PropertiesProvider;
+
 public class BaseTest {
+
+    @Test
+    public void testCase1(){
+        System.out.println(PropertiesProvider.getFromProperties("baseUrl"));
+    }
 }
